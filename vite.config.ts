6558297494dfdd2f549ceb83d60b6677b8804944
ryanmlow <ryanmlow00@gmail.com>
2025-05-01 13:21:26 +0800

    Add back base attribute and remove dist from gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ui-practice/",
   test: {
     globals: true,
     environment: "jsdom",
